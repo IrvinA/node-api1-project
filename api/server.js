73f42a81f27ee259310f6d01d236541e92cab720
IrvinA <arevalosirvin@gmail.com>
2021-11-29 09:39:39 -0800

    completed second get endpoint

--- api/server.js
+++ api/server.js
@@ -32,6 +32,24 @@ server.get('/api/users', async (req, res) => {
   }
 });
 
+server.get('/api/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (!user) {
+      res
+        .status(404)
+        .json({ message: 'The user with the specified ID does not exist' });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'The user information could not be retrieved' });
+  }
+});
+
 server.delete('/api/users/:id', async (req, res) => {
   try {
     const { id } = req.params;
