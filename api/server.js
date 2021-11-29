6741ce77be5ca80f6a4315eda49bc90b87ab0eb4
IrvinA <arevalosirvin@gmail.com>
2021-11-29 09:32:50 -0800

    Post endpoint complete

--- api/server.js
+++ api/server.js
@@ -3,4 +3,24 @@ const User = require('./users/model');
 const server = express();
 server.use(express.json());
 
+server.post('/api/users', async (req, res) => {
+  try {
+    const { name, bio } = req.body;
+    if (!name || !bio) {
+      res
+        .status(400)
+        .json({ message: 'Please provide name and bio for the user' });
+    } else {
+      const user = await User.insert({ name, bio });
+      res.status(201).json(user);
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: 'There was an error while saving the user to the database',
+      });
+  }
+});
+
 module.exports = server;
