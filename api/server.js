427e80abac5eb6d519c77516883c8b69c7742c22
IrvinA <arevalosirvin@gmail.com>
2021-11-29 09:36:15 -0800

    put endpoint complete

--- api/server.js
+++ api/server.js
@@ -48,4 +48,28 @@ server.delete('/api/users/:id', async (req, res) => {
   }
 });
 
+server.put('/api/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, bio } = req.body;
+    const user = await User.findById(id);
+    if (!user) {
+      res
+        .status(404)
+        .json({ message: 'The user with the specified ID does not exist' });
+    } else if (!name || !bio) {
+      res
+        .status(400)
+        .json({ message: 'Please provide name and bio for the user' });
+    } else {
+      const updateUser = await User.update(id, { name, bio });
+      res.status(200).json(updateUser);
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'The user information could not be modified' });
+  }
+});
+
 module.exports = server;
